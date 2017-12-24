199947ba552616a25fae0183276d34f308264858
Craig Ingram <cingram@us.ibm.com>
2017-12-23 20:57:39 -0500

    moar comments and refactoring for part1 and part2

--- src/main/scala/org/cinple/aoc/SpiralMemory.scala
+++ src/main/scala/org/cinple/aoc/SpiralMemory.scala
@@ -72,6 +72,8 @@ object CursorState {
   )
 }
 
+// pretty sure there is something wrong with this but the solution to this problem is symmetrical and
+// accommodated for some sloppiness here. could investigate but need to move on...
 object SpiralMemory extends aoc {
   private val input = 325489
 //  private val input = 68
@@ -90,17 +92,22 @@ object SpiralMemory extends aoc {
     case e => throw new Exception(s"Unhandled case $e")
   }
 
-  private def mkSpiral: (Array[Array[Int]], CursorState) = {
+  private def mkSpiral(part2: Boolean = false): (Array[Array[Int]], CursorState) = {
     val mat = Array.ofDim[Int](dim, dim)
-    val writer = new PrintWriter(new java.io.File("states"))
+    val writer = new PrintWriter(new java.io.File(s"spiral-states${if (part2) "2"}.txt"))
     val initState = CursorState(Point(hs, hs))
     var first = true
     var ans = true
-    val res = (1 to input).foldLeft(initState) { case (cs, _) =>
+    val res = (1 to input).foldLeft(initState) { case (cs, i) =>
       val v = if (first) { first = false; 1 } else sumAdjacent(cs.cursor, mat)
       if (v > input && ans) { println(s"~~~~: $v"); ans = false }
-      writer.write(s"$cs, $v\n")
-      mat(cs.cursor.c)(cs.cursor.r) = v
+      if (part2) {
+        writer.write(s"$cs, $v\n")
+        mat(cs.cursor.c)(cs.cursor.r) = v
+      } else {
+        writer.write(s"$cs, $i\n")
+        mat(cs.cursor.c)(cs.cursor.r) = i
+      }
       mvCursor(cs)
     }
     writer.close()
@@ -124,16 +131,13 @@ object SpiralMemory extends aoc {
     if (c.r + 1 <= dim) pts += Point(c.c, c.r + 1)
     if (c.r - 1 >= 0) pts += Point(c.c, c.r - 1)
 
-    val res = pts.foldLeft(0) { case (acc, pt) =>
-      acc + spiral(pt.c)(pt.r)
-    }
-    res
+    pts.foldLeft(0) { case (acc, pt) => acc + spiral(pt.c)(pt.r) }
   }
 
   override protected def part1(): Unit = {
-    val spiral = mkSpiral
+    val spiral = mkSpiral()
     println(s"final cursor state: (${spiral._2.cursor.c}, ${spiral._2.cursor.r}) ${spiral._2}")
-    val writer = new PrintWriter(new java.io.File("mat"))
+    val writer = new PrintWriter(new java.io.File("spiral-mat.txt"))
     spiral._1.foreach { rs =>
       rs.foreach { r => writer.write(f"$r%7d") }
       writer.write("\n")
@@ -142,5 +146,12 @@ object SpiralMemory extends aoc {
   }
 
   override protected def part2(): Unit = {
+    val spiral = mkSpiral(true)
+    val writer = new PrintWriter(new java.io.File("sprial-mat2.txt"))
+    spiral._1.foreach { rs =>
+      rs.foreach { r => writer.write(f"$r%7d") }
+      writer.write("\n")
+    }
+    writer.close()
   }
 }
